dca8ea2838b8fae9c548122977c8054caa7754b0
Rishabh Jaiswal <rishabhjaiswal225499@gmail.com>
2021-08-01 12:02:33 +0530

    fixed gitignore

--- src/main/ChessApplication.java
+++ src/main/ChessApplication.java
@@ -5,6 +5,7 @@ public class ChessApplication {
 	public static void main(String[] args) {
 		try {
 			ChessBoard.getPossibleCoordinates(args[0]);
+			System.out.print(false);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println(
 					"Please provide command line arguments as \"Peicename PeiceLocation\" example: \"King H5\".");
