90ad2574761557b38b36579d2fc9ab4a3786372a
Rishabh Jaiswal <rishabhjaiswal225499@gmail.com>
2021-08-01 12:03:21 +0530

    removed extra sys out

--- src/main/ChessApplication.java
+++ src/main/ChessApplication.java
@@ -5,7 +5,6 @@ public class ChessApplication {
 	public static void main(String[] args) {
 		try {
 			ChessBoard.getPossibleCoordinates(args[0]);
-			System.out.print(false);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println(
 					"Please provide command line arguments as \"Peicename PeiceLocation\" example: \"King H5\".");
